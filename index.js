afcb2d5cf6bf7ff6cf56fd1c05b6a4f17695a16c
rkumarsin <91483167+rkumarsin@users.noreply.github.com>
2023-04-14 22:31:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function handleTweetBtnClick(){
     if(tweetInput.value){
         tweetsData.unshift({
             handle: `@User`,
-            profilePic: `images/twitter.png`,
+            profilePic: `twitter.png`,
             likes: 0,
             retweets: 0,
             tweetText: tweetInput.value,
